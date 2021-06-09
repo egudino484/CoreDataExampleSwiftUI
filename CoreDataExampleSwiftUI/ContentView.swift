ecdf60c1701552b548df72c348f99023508d76b5
Edison Gudiño <egudino484@puce.edu.ec>
2021-06-09 16:33:14 -0500

    Initial Commit

---
+++ CoreDataExampleSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataExampleSwiftUI
+//
+//  Created by Edison Gudiño on 9/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreDataExampleSwiftUI/CoreDataExampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataExampleSwiftUIApp.swift
+//  CoreDataExampleSwiftUI
+//
+//  Created by Edison Gudiño on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataExampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
